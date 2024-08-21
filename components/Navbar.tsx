3bb468a2143e3bd7c29159baadc00bcd80b9fbc3
MananKateshiya <manankateshiya@gmail.com>
2024-08-21 18:19:21 +0530

    feat: Authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 function Navbar() {
   return (
-    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+    <nav className='flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
       <Link href="/"
         className='flex items-center gap-1'
       >
@@ -16,6 +18,13 @@ function Navbar() {
           className="max-sm:size-10" />
         <p className='text-[26px] font-extrabold max-sm:hidden text-white'>SquadCall</p>
       </Link>
+
+      <div className='flex-between gap-5'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <MobileNav />
+      </div>
     </nav>
   )
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,12 +15,12 @@ function Sidebar() {
         <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
             <div className="flex flex-1 flex-col gap-6">
                 {sidebarLinks.map((link) => {
-                    const isActive = pathname === link.route || pathname.startsWith(link.route);
+                    const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
                     return (
                         <Link href={link.route}
                             key={link.label}
                             className={
-                                cn('flex gap-4 items-center p-4 rounded-lg justify-start',
+                                cn('flex gap-4 items-center p-4 rounded-xl justify-start',
                                     { 'bg-blue-1': isActive, }
                                 )}>
                             <Image
@@ -30,6 +30,8 @@ function Sidebar() {
                                 alt={link.label} />
                                 <p className='text-lg font-semibold max-lg:hidden'>{link.label}</p>
                         </Link>
+
+                        
                     )
                 })}
             </div>
